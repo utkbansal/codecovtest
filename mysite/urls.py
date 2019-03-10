52e6831da1ea8ac1a36a224751d24b876f3babd5
Utkarsh Bansal <bansalutkarsh3@gmail.com>
2019-03-10 15:59:46 +0530

    Another test

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 
-from .views import foo_view, bar_view, hello_view
+from .views import foo_view, bar_view
 
 urlpatterns = [
     url(r'^foo/', foo_view, name='foo'),
     url(r'^bar/', bar_view, name='bar'),
-    url(r'^hello/', hello_view, name='hello'),
+    # url(r'^hello/', hello_view, name='hello'),
 ]
